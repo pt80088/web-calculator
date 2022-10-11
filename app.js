051640bdb67b45a59c12af4353f2ed11cb862b89
Toby Austin Locke <tobyaustinlocke@gmail.com>
2022-10-11 11:24:39 +0100

    second commit of in progress calculator web app, edits made to JavaScript calculation logic - first operation working, subsequent ones not working

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 let inputField = document.querySelectorAll("#input");
-let operators = document.querySelectorAll(".operators div");
+let operatorBtns = document.querySelectorAll(".operators div");
 let numbers = document.querySelectorAll(".numbers div");
 let equalsBtn = document.querySelectorAll(".equals")[0];
 let clearBtn = document.getElementById("clear");
 let decimalBtn = document.getElementById("decimal");
+let currentNumberDisplay = document.getElementsByClassName("current-number")[0];
 
 let currentNumber = 0;
 
-let currentCalculation = [];
+let calcFirstNumber = 0;
+let calcSecondNumber = 0;
+let calcOperator = "";
+
+// functions to convert operators from strings to operators
+
+let operators = {
+  "+": function (a, b) {
+    return a + b;
+  },
+  "*": function (a, b) {
+    return a * b;
+  },
+  "/": function (a, b) {
+    return a / b;
+  },
+  "-": function (a, b) {
+    return a - b;
+  },
+};
 
 // establishes event listener for number buttons
 for (let i = 0; i < numbers.length - 2; i++) {
@@ -21,8 +41,8 @@ for (let i = 0; i < numbers.length - 2; i++) {
 clearBtn.addEventListener("click", clear);
 
 // event listener for operator buttons
-for (let i = 0; i < operators.length; i++) {
-  operators[i].addEventListener("click", function (e) {
+for (let i = 0; i < operatorBtns.length; i++) {
+  operatorBtns[i].addEventListener("click", function (e) {
     let operator = e.target.textContent;
     operation(operator); // sends operator to operation function
     console.log(operator);
@@ -33,6 +53,7 @@ equalsBtn.addEventListener("click", calculate);
 
 decimalBtn.addEventListener("click", addDecimal);
 
+//event listener for keyboard input
 // event listener for equals or enter keys
 
 addEventListener("keypress", function (e) {
@@ -45,7 +66,7 @@ addEventListener("keypress", function (e) {
   }
 });
 
-//event listener for numbers
+// event listener for number keys
 
 for (let i = 0; i < 10; i++) {
   addEventListener("keypress", function (e) {
@@ -56,21 +77,30 @@ for (let i = 0; i < 10; i++) {
   });
 }
 
+//event listener for operator keys
+
 addEventListener("keypress", function (e) {
   if (e.key === "+" || e.key === "*" || e.key === "-" || e.key === "/") {
     operation(e.key);
   }
 });
 
-// operation function
-// (will need to store numerical value and operator awaiting further numerical value and equals sign)
-
 function operation(e) {
-  currentNumber = Number(inputField[0].value);
-  currentCalculation.push(currentNumber, e);
+  // if (calcFirstNumber === 0) {
+  //   calcFirstNumber = Number(inputField[0].value);
+  //   calcOperator = e;
+  //   inputField[0].value = "";
+  //   console.log("calcFirstNumber:" + calcFirstNumber);
+  // } else {
+  calcOperator = e;
+  calcFirstNumber = Number(inputField[0].value);
   inputField[0].value = "";
-  console.log(e.key);
-  console.log(currentCalculation);
+  currentNumber = operators[calcOperator](calcFirstNumber, currentNumber);
+  currentNumberDisplay.innerHTML = currentNumber;
+
+  // console.log(calcFirstNumber);
+  // console.log(calcOperator);
+  console.log("currentNumber:" + currentNumber);
 }
 
 // calculate function
@@ -98,6 +128,8 @@ function calculateSum() {
 
 function clear() {
   currentNumber = 0;
-  currentCalculation = [];
+  calcFirstNumber = 0;
+  calcSecondNumber = 0;
+  calcOperator = "";
   inputField[0].value = "";
 }
